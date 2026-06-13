92f8dd1b1db178a119284ebea5b5e29f4ead7d55
Gangalakshmi-R <glr16.engineer@gmail.com>
2026-06-13 14:29:49 +0530

    Commit

--- backend/src/main/java/com/gc/backend/config/CorsConfig.java
+++ backend/src/main/java/com/gc/backend/config/CorsConfig.java
@@ -21,7 +21,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
 
                         .allowedOrigins(
-                                "http://localhost:5173")
+                                "http://localhost:5173",
+                            "https://ganga-construction.onrender.com")
 
                         .allowedMethods("*")
 
